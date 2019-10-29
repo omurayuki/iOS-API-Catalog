4d35facd261c69bd801c10f430ad9c68a2015a2f
omurayuki <g1221438qz12@gmail.com>
2019-10-29 22:13:33 +0900

    coreNFCVCモック作成

--- iOS-API_Catalog/AppDelegate.swift
+++ iOS-API_Catalog/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         window.makeKeyAndVisible()
-        window.rootViewController = DocumentScanViewController()
+        window.rootViewController = CoreNFCViewController()
         return true
     }
 }
